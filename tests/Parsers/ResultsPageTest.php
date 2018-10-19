d46c61f2b0d9c4e021f4b8e4ecda18c4619fa9cc
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-10-19 20:54:38 +0300

    fix results

--- tests/Parsers/ResultsPageTest.php
+++ tests/Parsers/ResultsPageTest.php
@@ -46,7 +46,8 @@ class ResultsPageTest extends AbstractPageTest
                 'parameters' => null,
                 'splits' => [],
                 'status' => null,
-                'country' => 'Romania'
+                'country' => 'Romania',
+                'club' => null
             ],
             $results[5]->__toArray()
         );
@@ -84,60 +85,10 @@ class ResultsPageTest extends AbstractPageTest
 
         self::assertCount(20, $results);
         self::assertInstanceOf(Result::class, $results[18]);
+
+        $params = require TEST_FIXTURE_PATH . DS . 'Parsers' . DS . 'ResultsPage'. DS . 'event_page.php';
         self::assertEquals(
-            [
-                'posGen' => '39',
-                'bib' => '108',
-                'fullName' => '#vanatoriidehiene',
-                'href' => null,
-                'time' => '1:53:39.6',
-                'category' => null,
-                'posCategory' => '39',
-                'gender' => null,
-                'posGender' => null,
-                'id' => 'semimaraton-gerar-2/individual/-e713f42c94/108/',
-                'parameters' => null,
-                'splits' => [
-                    [
-                        'name' => 'round1',
-                        'time' => '0:17:05.8',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                    [
-                        'name' => 'round2',
-                        'time' => '0:18:23.2',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                    [
-                        'name' => 'round3',
-                        'time' => '0:19:06.3',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                    [
-                        'name' => 'round4',
-                        'time' => '0:20:08.3',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                    [
-                        'name' => 'round5',
-                        'time' => '0:19:35.2',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                    [
-                        'name' => 'round6',
-                        'time' => '0:19:20.8',
-                        'timeFromStart' => null,
-                        'parameters' => null
-                    ],
-                ],
-                'status' => null,
-                'country' => 'Romania'
-            ],
+            $params,
             $results[18]->__toArray()
         );
     }
---
+++ tests/fixtures/Parsers/ResultsPage/event_page.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+    'posGen' => '39',
+    'bib' => '108',
+    'fullName' => '#vanatoriidehiene',
+    'href' => null,
+    'time' => '1:53:39.6',
+    'category' => null,
+    'posCategory' => '39',
+    'gender' => null,
+    'posGender' => null,
+    'id' => 'semimaraton-gerar-2/individual/-e713f42c94/108/',
+    'parameters' => null,
+    'splits' => [
+        [
+            'name' => 'round1',
+            'time' => '0:17:05.8',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null
+        ],
+        [
+            'name' => 'round2',
+            'time' => '0:18:23.2',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null
+        ],
+        [
+            'name' => 'round3',
+            'time' => '0:19:06.3',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null
+        ],
+        [
+            'name' => 'round4',
+            'time' => '0:20:08.3',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null
+        ],
+        [
+            'name' => 'round5',
+            'time' => '0:19:35.2',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null,
+        ],
+        [
+            'name' => 'round6',
+            'time' => '0:19:20.8',
+            'timeFromStart' => null,
+            'parameters' => null,
+            'timeOfDay' => null,
+            'posGen' => null,
+            'posCategory' => null,
+            'posGender' => null,
+        ],
+    ],
+    'status' => null,
+    'country' => 'Romania',
+    'club' => null
+];
